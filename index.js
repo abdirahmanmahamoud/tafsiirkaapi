6937d80545fb25c4a3344f80cd55a78d5f500e91
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2024-06-24 21:57:30 +0300

    add cors

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ import express from "express";
 import maxamedIsmaaciil from "./routes/maxamed-ismaaciil.js";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import cors from "cors";
 
 const app = express();
+
+const corsOptions = {
+  origin: "*",
+};
+
+app.use(cors(corsOptions));
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 app.use("/images", express.static(__dirname + "/images"));
